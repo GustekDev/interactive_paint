830d589ea826cbbaf013b575175800861b4278b4
GustekDev <panguzol@gmail.com>
2023-08-17 11:26:52 +0100

    paint widget

---
+++ lib/paint_widget.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/widgets.dart';
+
+class PaintWidget extends StatelessWidget {
+  const PaintWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
